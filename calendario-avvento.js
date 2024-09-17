531f29137879bfd5689397dd68da6a4e0c810cc1
ilastraz <38754098+ilastraz@users.noreply.github.com>
2024-09-17 21:43:48 +0200

    v2 - popup

--- calendario-avvento.js
+++ calendario-avvento.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
   function updateCalendar(currentDay) {
     const divs = document.querySelectorAll("div[data-day]");
@@ -11,6 +10,8 @@ document.addEventListener("DOMContentLoaded", function() {
         div.style.display = "block";
       } else if (day === currentDay && status === "today") {
         div.style.display = "block";
+        div.style.cursor = "pointer";
+        div.addEventListener('click', () => openPopup(day));
       } else if (day > currentDay && status === "future") {
         div.style.display = "block";
       } else {
@@ -34,10 +35,46 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
+  // Funzione per aprire il popup
+  function openPopup(day) {
+    fetch('https://corsproxy.io/?https://raw.githubusercontent.com/ilastraz/AdventCalendar/main/calendario-contenuti.json')
+      .then(response => response.json())
+      .then(data => {
+        const dayData = data[day.toString()];
+        if (dayData) {
+          document.querySelector('.popup-date').textContent = dayData.data;
+          document.querySelector('.popup-head').textContent = dayData.head;
+          document.querySelector('.popup-description').textContent = dayData.descrizione;
+          document.querySelector('.popup-cta1').href = dayData.cta1.link;
+          document.querySelector('.popup-cta1').textContent = dayData.cta1.testo;
+          document.querySelector('.popup-cta2').href = dayData.cta2.link;
+          document.querySelector('.popup-cta2').textContent = dayData.cta2.testo;
+          
+          const popupContent = document.querySelector('.popup-content');
+          if (window.innerWidth > 768) {
+            popupContent.style.backgroundImage = `url(${dayData.bgdesktop})`;
+          } else {
+            popupContent.style.backgroundImage = `url(${dayData.bgmobile})`;
+          }
+
+          document.querySelector('.popup').style.display = 'flex';
+        }
+      })
+      .catch(error => console.error('Errore nel caricamento dei dati:', error));
+  }
+
+  // Funzione per chiudere il popup
+  function closePopup() {
+    document.querySelector('.popup').style.display = 'none';
+  }
+
   // Esegui l'aggiornamento all'avvio
   fetchCurrentDay();
 
   // Imposta un timer per eseguire l'aggiornamento ogni giorno
   setInterval(fetchCurrentDay, 24 * 60 * 60 * 1000);
+
+  // Aggiungi l'event listener per chiudere il popup
+  document.querySelector('.popup-close').addEventListener('click', closePopup);
 });
 
