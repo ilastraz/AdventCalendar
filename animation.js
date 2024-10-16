d6410dfe2a385f5209015153e937c8d595599889
ilastraz <38754098+ilastraz@users.noreply.github.com>
2024-10-16 11:12:16 +0200

    snow fix

--- animation.js
+++ animation.js
@@ -51,11 +51,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 
 // SNOW
-
-// Snow from https://codepen.io/radum/pen/xICAB
-
 (function () {
-
     var COUNT = 300;
     var masthead = document.querySelector('body');
     var canvas = document.createElement('canvas');
@@ -64,88 +60,89 @@ window.addEventListener("DOMContentLoaded", (event) => {
     var height = masthead.clientHeight;
     var i = 0;
     var active = false;
-  
+
     function onResize() {
-      width = masthead.clientWidth;
-      height = masthead.clientHeight;
-      canvas.width = width;
-      canvas.height = height;
-      ctx.fillStyle = '#FFF';
-  
-      var wasActive = active;
-      active = width > 600;
-  
-      if (!wasActive && active)
-        requestAnimFrame(update);
+        width = masthead.clientWidth;
+        height = masthead.clientHeight;
+        canvas.width = width;
+        canvas.height = height;
+        ctx.fillStyle = '#FFF';
+
+        var wasActive = active;
+        active = width > 600;
+
+        if (!wasActive && active)
+            requestAnimFrame(update);
     }
-  
+
     var Snowflake = function () {
-      this.x = 0;
-      this.y = 0;
-      this.vy = 0;
-      this.vx = 0;
-      this.r = 0;
-  
-      this.reset();
+        this.x = 0;
+        this.y = 0;
+        this.vy = 0;
+        this.vx = 0;
+        this.r = 0;
+
+        this.reset();
     }
-  
-    Snowflake.prototype.reset = function() {
-      this.x = Math.random() * width;
-      this.y = Math.random() * -height;
-      this.vy = 1 + Math.random() * 3;
-      this.vx = 0.5 - Math.random();
-      this.r = 1 + Math.random() * 2;
-      this.o = 0.5 + Math.random() * 0.5;
+
+    Snowflake.prototype.reset = function () {
+        this.x = Math.random() * width;
+        // Inizializza la posizione 'y' in modo che alcuni fiocchi siano già visibili
+        this.y = Math.random() * height;
+        this.vy = 1 + Math.random() * 3;
+        this.vx = 0.5 - Math.random();
+        this.r = 1 + Math.random() * 2;
+        this.o = 0.5 + Math.random() * 0.5;
     }
-  
+
     canvas.style.position = 'absolute';
     canvas.style.left = canvas.style.top = '0';
-  
+    canvas.style.pointerEvents = 'none'; // Permette il click sugli elementi sottostanti
+
     var snowflakes = [], snowflake;
     for (i = 0; i < COUNT; i++) {
-      snowflake = new Snowflake();
-      snowflake.reset();
-      snowflakes.push(snowflake);
+        snowflake = new Snowflake();
+        snowflake.reset();
+        snowflakes.push(snowflake);
     }
-  
+
     function update() {
-  
-      ctx.clearRect(0, 0, width, height);
-  
-      if (!active)
-        return;
-  
-      for (i = 0; i < COUNT; i++) {
-        snowflake = snowflakes[i];
-        snowflake.y += snowflake.vy;
-        snowflake.x += snowflake.vx;
-  
-        ctx.globalAlpha = snowflake.o;
-        ctx.beginPath();
-        ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
-        ctx.closePath();
-        ctx.fill();
-  
-        if (snowflake.y > height) {
-          snowflake.reset();
+        ctx.clearRect(0, 0, width, height);
+
+        if (!active)
+            return;
+
+        for (i = 0; i < COUNT; i++) {
+            snowflake = snowflakes[i];
+            snowflake.y += snowflake.vy;
+            snowflake.x += snowflake.vx;
+
+            ctx.globalAlpha = snowflake.o;
+            ctx.beginPath();
+            ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.fill();
+
+            if (snowflake.y > height) {
+                snowflake.reset();
+            }
         }
-      }
-  
-      requestAnimFrame(update);
+
+        requestAnimFrame(update);
     }
-  
+
     // shim layer with setTimeout fallback
-    window.requestAnimFrame = (function(){
-      return  window.requestAnimationFrame       ||
-              window.webkitRequestAnimationFrame ||
-              window.mozRequestAnimationFrame    ||
-              function( callback ){
+    window.requestAnimFrame = (function () {
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            function (callback) {
                 window.setTimeout(callback, 1000 / 60);
-              };
+            };
     })();
-  
+
     onResize();
     window.addEventListener('resize', onResize, false);
-  
+
     masthead.appendChild(canvas);
-  })();
+})();
