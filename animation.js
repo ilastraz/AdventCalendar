a8c433ee4ecf7b3878dd1667ca82e28562130918
ilastraz <38754098+ilastraz@users.noreply.github.com>
2024-10-25 10:52:52 +0200

    gsap fixed

--- animation.js
+++ animation.js
@@ -116,9 +116,9 @@ window.addEventListener("load", function() {
   
     // Anima intro-fondo, intro-albero1, intro-albero4, intro-albero2, intro-albero3, e intro-neve insieme
     IntroTl.to(".intro-fondo", { z: "0rem", duration: 1, ease: "power4.out" }, 0);
-    IntroTl.to([".intro-albero1", ".intro-albero4"], { z: "0rem", scale: 1.2, duration: 1, ease: "expo.out" }, 0);
-    IntroTl.to([".intro-albero2", ".intro-albero3"], { z: "0rem", scale: 1.2, duration: 1, ease: "expo.out" }, 0);
-    IntroTl.to(".intro-neve", { z: "0rem", duration: 1, ease: "expo.in" }, 0.1);
+    IntroTl.to([".intro-albero1", ".intro-albero4"], { z: "0rem", scale: 1.2, duration: 1.1, ease: "expo.out" }, 0);
+    IntroTl.to([".intro-albero2", ".intro-albero3"], { z: "0rem", scale: 1.2, duration: 1.1, ease: "expo.out" }, 0);
+    IntroTl.to(".intro-neve", { z: "0rem", duration: 1, ease: "expo.out" }, 0.1);
 
     // Al termine della IntroTl, mostra la sezione caselle dopo 1 secondo
     IntroTl.call(function() {
