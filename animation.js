38138a47f2e9f979cab35ab54615597253e3a377
ilastraz <38754098+ilastraz@users.noreply.github.com>
2024-10-31 17:27:55 +0100

    updare intro

--- animation.js
+++ animation.js
@@ -128,7 +128,7 @@ window.addEventListener("load", function () {
       ".intro-albero3",
       ".intro-neve",
     ],
-    { z: "100rem", autoAlpha: 1 }
+    { transform: "translateZ(100rem)", autoAlpha: 0 }
   );
 
   // Nascondi il testo prima che inizi l'animazione
@@ -138,18 +138,18 @@ window.addEventListener("load", function () {
   let IntroTl = gsap.timeline();
 
   // Anima intro-fondo, intro-albero1, intro-albero4, intro-albero2, intro-albero3, e intro-neve insieme
-  IntroTl.to(".intro-fondo", { z: "0rem", duration: 2, ease: "power4.out" }, 0);
+  IntroTl.to(".intro-fondo", { transform: "translateZ(0rem)", autoAlpha: 1, duration: 2, ease: "power4.out" }, 0);
   IntroTl.to(
     [".intro-albero1", ".intro-albero4"],
-    { z: "0rem", scale: 1.2, duration: 2.1, ease: "expo.out" },
+    { transform: "translateZ(0rem) scale(1.2)", autoAlpha: 1, duration: 2.1, ease: "expo.out" },
     0
   );
   IntroTl.to(
     [".intro-albero2", ".intro-albero3"],
-    { z: "0rem", scale: 1.2, duration: 2.1, ease: "expo.out" },
+    { transform: "translateZ(0rem) scale(1.2)", autoAlpha: 1, duration: 2.1, ease: "expo.out" },
     0
   );
-  IntroTl.to(".intro-neve", { z: "0rem", duration: 2, ease: "expo.out" }, 0.1);
+  IntroTl.to(".intro-neve", { transform: "translateZ(0rem)", autoAlpha: 1, duration: 2, ease: "expo.out" }, 0.1);
 
   // Al termine della IntroTl, mostra la sezione caselle dopo 1 secondo
   IntroTl.call(function () {
@@ -167,8 +167,8 @@ window.addEventListener("load", function () {
   setTimeout(function () {
     $("[letters-slide-up]").each(function (index) {
       let tl = gsap.timeline({ paused: false });
-      tl.set($(this), { autoAlpha: 1 }); // Rendi visibile il testo
-      tl.from($(this).find(".char"), { yPercent: 100, duration: 0.2, ease: "power1.out", stagger: { amount: 0.6 } });
+      tl.set($(this), { autoAlpha: 0 });
+      tl.fromTo($(this).find(".char"), { autoAlpha: 0, yPercent: 100 }, { autoAlpha: 1, yPercent: 0, duration: 0.2, ease: "power1.out", stagger: { amount: 0.6 } });
     });
   }, 1000);
 
@@ -176,8 +176,8 @@ window.addEventListener("load", function () {
   setTimeout(function () {
     $("[letters-slide-down]").each(function (index) {
       let tl = gsap.timeline({ paused: false });
-      tl.set($(this), { autoAlpha: 1 }); // Rendi visibile il testo
-      tl.from($(this).find(".char"), { yPercent: -120, duration: 0.3, ease: "power1.out" }, 0.1);
+      tl.set($(this), { autoAlpha: 0 });
+      tl.fromTo($(this).find(".char"), { autoAlpha: 0, yPercent: -120 }, { autoAlpha: 1, yPercent: 0, duration: 0.3, ease: "power1.out" }, 0.1);
     });
   }, 1500);
 
@@ -205,19 +205,20 @@ window.addEventListener("load", function () {
         ".intro-alberi-mobile",
         ".intro-neve-mobile",
       ],
-      { y: "100%", autoAlpha: 1 } // Sposta gli elementi inizialmente fuori dallo schermo sull'asse Y e rendili invisibili
+      { transform: "translateY(100%)", autoAlpha: 0 }
     );
 
     // Creazione di una timeline per le animazioni mobile
     let IntroMobileTl = gsap.timeline();
 
     // Anima intro-fondo-mobile, intro-alberi-mobile, e intro-neve-mobile insieme
-    IntroMobileTl.to(".intro-fondo-mobile", { y: "0%", duration: 2, ease: "power4.out" }, 0);
+    IntroMobileTl.to(".intro-fondo-mobile", { transform: "translateY(0%)", autoAlpha: 1, duration: 2, ease: "power4.out" }, 0);
     IntroMobileTl.to(
       ".intro-alberi-mobile",
-      { y: "0%", scale: 1.2, duration: 2.1, ease: "expo.out" },
+      { transform: "translateY(0%) scale(1.2)", autoAlpha: 1, duration: 2.1, ease: "expo.out" },
       0
     );
-    IntroMobileTl.to(".intro-neve-mobile", { y: "0%", duration: 2, ease: "expo.out" }, 0.1);
+    IntroMobileTl.to(".intro-neve-mobile", { transform: "translateY(0%)", autoAlpha: 1, duration: 2, ease: "expo.out" }, 0.1);
   }
 });
+
